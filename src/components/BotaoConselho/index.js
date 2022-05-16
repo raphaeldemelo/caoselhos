fb437e59f11e8482810016e6d26455c16f83403f
Raphael de Melo <raphaeldemelosoares@hotmail.com>
2022-05-16 18:27:01 -0300

    feat: criando componente botão

---
+++ src/components/BotaoConselho/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './styles.css';
+
+export default function BotaoConselho() {
+    return (
+        <button class="learn-more">
+            <span class="circle">
+                <span class="icon arrow"></span>
+            </span>
+            <span class="button-text">Pedir conselho</span>
+        </button>
+    );
+}
