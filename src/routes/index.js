a94ce07bf1fce547f0e4847b89e76c0a5d429aa9
Raphael de Melo <raphaeldemelosoares@hotmail.com>
2022-05-16 18:24:11 -0300

    feat: criando rota da página conselhos

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
 
 import Home from '../Pages/Home';
+import Conselhos from '../Pages/Conselhos';
 
 export default function routes() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Home />} />
+                <Route path='/conselhos' element={<Conselhos />} />
             </Routes>
         </BrowserRouter>
     );
